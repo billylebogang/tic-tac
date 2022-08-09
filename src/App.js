ef431574ff09b2bde529dbfc285bcd01bef126de
Billi <55185706+billylebogang@users.noreply.github.com>
2022-08-09 03:25:40 +0200

    Update App.js
    checking for  changes rendering

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <p>Billy tic-tac-toe</p>
+       <p>Billy Lebogang tic-tac-toe</p>
       </header>
       <Game />
     </div>
