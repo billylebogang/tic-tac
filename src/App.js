b223ca5aa4a85784eb4f92be93ffc515403227c4
Billi <55185706+billylebogang@users.noreply.github.com>
2022-08-09 02:56:18 +0200

    first commit if the tic-tac
    the games functioning

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import {Game} from './componets/Game';
 import './App.css';
+
+
+
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <p>Billy tic-tac-toe</p>
       </header>
+      <Game />
     </div>
   );
 }
